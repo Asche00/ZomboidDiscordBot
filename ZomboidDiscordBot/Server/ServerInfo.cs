250fb7a3a2f9b3fe1651c4f38c61e567d934214e
Asche <82905226+Asche00@users.noreply.github.com>
2023-03-09 10:04:55 -0600

    Added ServerInfo variables.

---
+++ ZomboidDiscordBot/Server/ServerInfo.cs
@@ -0,0 +1,16 @@
+﻿using Discord.Rest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZomboidTypesLibrary;
+
+namespace ZomboidDiscordBot.Server
+{
+    public static class ServerInfo
+    {
+        public static ZomboidTypesLibrary.ZomboidTypeServerState.ServerState ServerState = ZomboidTypeServerState.ServerState.Offline;
+        public static int PlayerCount = 0;
+    }
+}
