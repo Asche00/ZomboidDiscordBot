e19367cef72cb8cfd7188cbbae4f3904aa32bfd7
Asche <82905226+Asche00@users.noreply.github.com>
2023-03-09 10:06:24 -0600

    Clean up.

--- ZomboidDiscordBot/Server/ServerInfo.cs
+++ ZomboidDiscordBot/Server/ServerInfo.cs
@@ -1,16 +1,10 @@
-﻿using Discord.Rest;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using ZomboidTypesLibrary;
+﻿using ZomboidTypesLibrary;
 
 namespace ZomboidDiscordBot.Server
 {
     public static class ServerInfo
     {
-        public static ZomboidTypesLibrary.ZomboidTypeServerState.ServerState ServerState = ZomboidTypeServerState.ServerState.Offline;
+        public static ZomboidTypeServerState.ServerState ServerState = ZomboidTypeServerState.ServerState.Offline;
         public static int PlayerCount = 0;
     }
 }
