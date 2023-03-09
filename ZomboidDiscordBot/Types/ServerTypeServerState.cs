61c14984495f11baf022ecb70f14c91bdfefaefd
Asche <82905226+Asche00@users.noreply.github.com>
2023-03-09 09:28:25 -0600

    Create ServerTypeServerState.cs
    Added server status enum

---
+++ ZomboidDiscordBot/Types/ServerTypeServerState.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace ZomboidTypesLibrary
+{
+    public partial class ServerTypeServerState
+    {
+        //Enums
+        public enum ServerState
+        {
+            [Description("Offline")]
+            Offline = 0,
+            [Description("Online")]
+            Online = 1,
+            [Description("Restarting")]
+            Restart = 2,
+            [Description("Unknown")]
+            Unknown = 3
+        }
+    }
+}
